f7b807da8a3783101f8f5864430133c6a922db26
Osahon Oboite <osahonoboite@gmail.com>
2025-07-25 17:22:40 +0100

    chore: format

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,37 +3,34 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import {
-  transformerNotationDiff,
-  transformerNotationFocus,
-  transformerMetaHighlight
+	transformerNotationDiff,
+	transformerNotationFocus,
+	transformerMetaHighlight,
 } from '@shikijs/transformers';
 // @ts-ignore
-import remarkCodeTitles from "remark-code-titles";
+import remarkCodeTitles from 'remark-code-titles';
 import remarkMath from 'remark-math';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+	site: 'https://example.com',
 
-  integrations: [
-    mdx(),
-    sitemap()
-  ],
+	integrations: [mdx(), sitemap()],
 
-  markdown: {
-    syntaxHighlight: 'shiki',
-	  remarkPlugins: [remarkCodeTitles, remarkMath],
-    // rehypePlugins: [rehypeKatex],
-    shikiConfig: {
-      themes: {
-        light: 'github-light',
-        dark: 'github-dark'
-      },
-      transformers: [
-        transformerNotationDiff(),
-        transformerNotationFocus(),
-        transformerMetaHighlight()
-      ]
-    }
-  }
+	markdown: {
+		syntaxHighlight: 'shiki',
+		remarkPlugins: [remarkCodeTitles, remarkMath],
+		// rehypePlugins: [rehypeKatex],
+		shikiConfig: {
+			themes: {
+				light: 'github-light',
+				dark: 'github-dark',
+			},
+			transformers: [
+				transformerNotationDiff(),
+				transformerNotationFocus(),
+				transformerMetaHighlight(),
+			],
+		},
+	},
 });
--- src/content.config.ts
+++ src/content.config.ts
@@ -1,19 +1,22 @@
-import { glob } from "astro/loaders";
-import { defineCollection, z } from "astro:content";
+import { glob } from 'astro/loaders';
+import { defineCollection, z } from 'astro:content';
 
 const article = defineCollection({
-  loader: glob({ base: "./src/content/articles", pattern: "**/*.{md,mdx}" }),
-  schema: () =>
-    z.object({
-      title: z.string(),
-      description: z.string(),
-      pubDate: z.coerce.date(),
-      slug: z.string(),
-      readTime: z.number().optional(),
-      updatedDate: z.coerce.date().optional(),
-      external: z.boolean().optional(),
-      next: z.string().optional(),
-      previous: z.string().optional()
-    }),
+	loader: glob({
+		base: './src/content/articles',
+		pattern: '**/*.{md,mdx}',
+	}),
+	schema: () =>
+		z.object({
+			title: z.string(),
+			description: z.string(),
+			pubDate: z.coerce.date(),
+			slug: z.string(),
+			readTime: z.number().optional(),
+			updatedDate: z.coerce.date().optional(),
+			external: z.boolean().optional(),
+			next: z.string().optional(),
+			previous: z.string().optional(),
+		}),
 });
 export const collections = { article };
